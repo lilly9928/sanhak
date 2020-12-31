128ca3f655ccb4431f77b62bf63e436fcca601ad
lilly9928 <50159938+lilly9928@users.noreply.github.com>
2020-12-31 10:45:49 +0900

    메인디자인기본마무리

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,53 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//iso9001
+router.get('/iso9001_sum', function(req, res, next) {
+  res.render('./iso9001/summary', { title: '에이원경영' });
+});
+router.get('/iso9001_step', function(req, res, next) {
+  res.render('./iso9001/step', { title: '에이원경영' });
+});
+router.get('/iso9001_ben', function(req, res, next) {
+  res.render('./iso9001/benefit', { title: '에이원경영' });
+});
+
+//iso14001
+router.get('/iso14001_sum', function(req, res, next) {
+  res.render('./iso14001/summary', { title: '에이원경영' });
+});
+router.get('/iso14001_step', function(req, res, next) {
+  res.render('./iso14001/step', { title: '에이원경영' });
+});
+router.get('/iso14001_ben', function(req, res, next) {
+  res.render('./iso14001/benefit', { title: '에이원경영' });
+});
+
+//laboratory
+router.get('/laboratory_sum', function(req, res, next) {
+  res.render('./laboratory/summary', { title: '에이원경영' });
+});
+router.get('/laboratory_step', function(req, res, next) {
+  res.render('./laboratory/step', { title: '에이원경영' });
+});
+router.get('/laboratory_ben', function(req, res, next) {
+  res.render('./laboratory/benefit', { title: '에이원경영' });
+});
+
+//product
+router.get('/product_sum', function(req, res, next) {
+  res.render('./product/summary', { title: '에이원경영' });
+});
+router.get('/product_ben', function(req, res, next) {
+  res.render('./product/benefit', { title: '에이원경영' });
+});
+
+//enterpraise
+router.get('/enterpraise_sum', function(req, res, next) {
+  res.render('./enterpraise/summary', { title: '에이원경영' });
+});
+router.get('/enterpraise_ben', function(req, res, next) {
+  res.render('./enterpraise/benefit', { title: '에이원경영' });
+});
+
 module.exports = router;
--- myapp/routes/views.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* dashboard page. */
-router.get('/', function(req, res, next) {
-  res.render('main', { title: '에이원경영' });
-});
-module.exports = router;
